e3da6d6c44a3eb029ebfea43ea7b1ffc165103cc
lizardon <donchia@ymail.com>
2023-09-01 18:07:17 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<qrcode-stream></qrcode-stream>
+<qrcode-drop-zone></qrcode-drop-zone>
+<qrcode-capture></qrcode-capture>
+</template>
+
+<script setup>
+import {
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture
+} from 'vue-qrcode-reader'
+</script>
