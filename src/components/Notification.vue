b6f64f47d27557bea4d772c536f971bc35373d63
lizardon <donchia@ymail.com>
2023-09-02 18:27:15 +0800

    Update Notification.vue

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -12,7 +12,7 @@
                         </div>
                         <div class="ml-3 w-0 flex-1 pt-0.5">
                             <p class="text-sm font-medium text-gray-900">{{badgesStore.recentlyCollected.name}}'s Badge Collected!</p>
-                            <p class="mt-1 text-sm text-gray-500">{{ checkHowManyBadgesLeft == 0 ? 'Congratulations! All badges have been collected! Head over to our booth to collect your prize!' : checkHowManyBadgesLeft + ' more badges to go!'}}</p>
+                            <!-- <p class="mt-1 text-sm text-gray-500">{{ checkHowManyBadgesLeft == 0 ? 'Congratulations! All badges have been collected! Head over to our booth to collect your prize!' : checkHowManyBadgesLeft + ' more badges to go!'}}</p> -->
                         </div>
                         <div class="ml-4 flex flex-shrink-0">
                             <button type="button" @click="show = false" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
