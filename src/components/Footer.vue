284a739af55d2be47eee04ccc21d535ec2a90958
lizardon <donchia@ymail.com>
2023-09-02 18:19:22 +0800

    badges

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
 <footer>
     <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
         <div class="flex justify-center space-x-2 md:order-2">
-            <div class="bg-gray-200 rounded-full w-8 h-8"></div>
-            <div class="bg-gray-200 rounded-full w-8 h-8"></div>
+            <img src="../assets/icons/ejc.png" class="rounded-full w-12"/>
+            <div class="bg-gray-200 rounded-full w-12 h-12"></div>
         </div>
         <div class="mt-4 md:order-1 flex flex-col space-y-4 items-center">
             <p class="text-center text-xs leading-5 text-gray-500">&copy; {{new Date().getFullYear()}} 10C4I x Eunoia JC</p>
