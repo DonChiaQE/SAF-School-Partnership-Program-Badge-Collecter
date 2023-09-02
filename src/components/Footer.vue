dc37f8bf6fdb9cdba6c800b9c029f22f3e549ad1
lizardon <donchia@ymail.com>
2023-09-02 19:26:53 +0800

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
 <footer>
     <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div class="flex justify-center space-x-2 md:order-2">
-            <img src="../assets/icons/ejc.png" class="rounded-full w-12"/>
-            <div class="bg-gray-200 rounded-full w-12 h-12"></div>
+        <div class="flex justify-center space-x-3 md:order-2">
+            <img src="../assets/icons/ejc.png" class="w-12"/>
+            <img src="../assets/icons/10c4i.png" class="w-12"/>
         </div>
         <div class="mt-4 md:order-1 flex flex-col space-y-4 items-center">
             <p class="text-center text-xs leading-5 text-gray-500">&copy; {{new Date().getFullYear()}} 10C4I x Eunoia JC</p>
