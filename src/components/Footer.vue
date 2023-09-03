6df05087f3340b539493a678fe28b5b3f17cb12f
lizardon <donchia@ymail.com>
2023-09-03 12:59:50 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
 <footer>
     <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
         <div class="flex justify-center space-x-3 md:order-2">
-            <img src="../assets/icons/ejc.png" class="w-12"/>
             <img src="../assets/icons/10c4i.png" class="w-12"/>
+            <img src="../assets/icons/ejc.png" class="w-12"/>
         </div>
         <div class="mt-4 md:order-1 flex flex-col space-y-4 items-center">
             <p class="text-center text-xs leading-5 text-gray-500">SSPP - 10 C4I Bn x Eunoia Junior College (7 Sep 23)</p>
