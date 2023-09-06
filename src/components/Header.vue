f8a64ddf2c823f8781d07e04f7ec0679a6acdb97
lizardon <donchia@ymail.com>
2023-09-06 23:23:12 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 <div class="mx-auto max-w-3xl w-full mb-8">
     <p class="text-xl font-light">Welcome,</p>
     <h1 class="text-4xl font-bold">REC {{ nameStore.name }}</h1>
-    <p class="text-xl font-light">SSPP 10C4I Bn x EJC</p>
+    <p class="text-xl font-light">SSPP 10 C4I Bn x EJC</p>
 </div>
 </template>
 
