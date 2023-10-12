357c94c28ca917c6d71cbb7b61c5ba3ff86f4732
lizardon <donchia@ymail.com>
2023-10-12 09:36:42 +0800

    truncate

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="mx-auto max-w-3xl w-full mb-8">
     <p class="text-xl font-light">Welcome,</p>
-    <h1 class="text-4xl font-bold">REC {{ nameStore.name }}</h1>
+    <h1 class="text-4xl font-bold truncate">REC {{ nameStore.name }}</h1>
     <p class="text-xl font-light">SSPP 10 C4I Bn x EJC</p>
 </div>
 </template>
